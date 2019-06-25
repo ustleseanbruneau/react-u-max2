2c3d64f330ce46b5ad0f815f9ac815c76fba64de
MN JS Dude <ustbrun9912@gmail.com>
2019-06-24 21:38:10 -0500

    Lecture 94 - thru useEffect()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,31 +1,49 @@
-import React from 'react';
+//import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
-const cockpit = (props) => {
-    const assignedClasses = [];
-    let btnClass = '';
+const Cockpit = props => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000);
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);  //empty array only runs first time
 
-    if (props.showPersons) {
-        btnClass = classes.Red;
-    }
-    
-    if(props.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-    if(props.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-    }
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return() => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
 
-    return (
-        <div className={classes.Cockpit}>
-        <h1>{props.title}</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button
-          className={btnClass}
-          onClick={props.clicked}>Toggle Persons</button>
-        </div>
-    );
+  const assignedClasses = [];
+  let btnClass = '';
+
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button
+        className={btnClass}
+        onClick={props.clicked}>Toggle Persons</button>
+    </div>
+  );
 };
 
-export default cockpit;
+export default Cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -22,7 +22,8 @@ class App extends Component {
       { id: 'cads', name: 'Tom', age: 53 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -92,13 +93,14 @@ class App extends Component {
     }
 
     return (
-
         <div className={classes.App}>
-          <Cockpit 
+          <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
+          {this.state.showCockpit ? <Cockpit 
             title={this.props.appTitle}
             showPersons={this.state.showPersons}
             persons={this.state.persons}
-            clicked={this.togglePersonsHandler} />
+            clicked={this.togglePersonsHandler} 
+          /> : null}
           {persons}
         </div>
     );
