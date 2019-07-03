64c1dad72f3e7611bc7d63aab62d24bdee24f938
MN JS Dude <ustbrun9912@gmail.com>
2019-07-02 21:47:40 -0500

    Lecture 102 - Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
@@ -7,12 +7,14 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
 
+        // Replace <Aux> with <Fragment> or <React.Fragment> without import
+
         return (
-            <Aux>
+            <Fragment>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key="i2">{this.props.children}</p>
                 <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </Fragment>
         );
     }
 }
